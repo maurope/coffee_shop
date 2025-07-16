b99ef761b5c1ce9c7542923450dfb0b53ba3cc21
maurope <mauricio.penuela@hotmail.com>
2025-07-16 07:29:13 -0500

    Code update

---
+++ coffee_shop/products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.TextField(max_length=200, verbose_name="Nombre")
+    description = models.TextField(max_length=300, verbose_name="Descripción")
+    price = models.DecimalField(max_digits=2, decimal_places=2, verbose_name="Precio")
+    available = models.BooleanField(default=True, verbose_name='Disponible') 
+    photo =models.ImageField(upload_to='logos', null=True, blank=True, verbose_name='Foto')
+
+    def __str__(self):
+        return self.name
