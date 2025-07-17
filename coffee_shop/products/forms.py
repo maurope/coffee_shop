e798a83036e14dc877eea1ac738b33445ece6828
maurope <mauricio.penuela@hotmail.com>
2025-07-17 10:21:51 -0500

    Code update

--- coffee_shop/products/forms.py
+++ coffee_shop/products/forms.py
@@ -5,7 +5,7 @@ from .models import Product
 class ProductForm(forms.Form):
     name = forms.CharField(max_length=200, label="Nombre")
     description = forms.CharField(max_length=300, label="Descripción")
-    price = forms.DecimalField(max_digits=10, decimal_places=2, label="Precio")
+    price = forms.DecimalField(max_digits= 10, decimal_places=2, label="Precio")
     available = forms.BooleanField(initial=True,label='Disponible', required=False)
     photo = forms.ImageField(label='Foto', required=False)  
 
