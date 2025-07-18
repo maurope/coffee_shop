8d67f6ed34644c43f2fa2db1ef03266b88e91e42
maurope <mauricio.penuela@hotmail.com>
2025-07-18 09:55:53 -0500

    html update

--- coffee_shop/products/views.py
+++ coffee_shop/products/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import ProductForm  
+from .forms import ProductForm
+from .models import Product
 
 
 # Create your views here.
@@ -13,3 +14,8 @@ class ProductFormView(generic.FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+    
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list_product.html'
+    context_object_name = 'products'
